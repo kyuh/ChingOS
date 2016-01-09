ad674bf3815915ba63587f60b8b113add1246234
Andrew Zhao <azhao@dmail.caltech.edu>
2016-01-08 22:29:08 -0800

    initial working version

--- src/shell/child_process.c
+++ src/shell/child_process.c
@@ -4,8 +4,8 @@
 #include <shell_utils.h>
 
 typedef struct {
-    int input;
     int output;
+    int input;
 } Pipe;
 
 /*
@@ -18,16 +18,16 @@ typedef struct {
  * 
  * (recieve data from input, output it into output)
  */
-void forkChild(char **argv, Pipe *inputStream, Pipe *outputStream) {
+void forkChild(char **argv, const Pipe inputStream, const Pipe outputStream) {
     //don't do anything if we're the child process
     if(fork()) {
         //set input and output of the process
-        dup2(inputStream->output, STDIN_FILENO);
-        dup2(outputStream->input, STDOUT_FILENO);
+        dup2(inputStream.output, STDIN_FILENO);
+        dup2(outputStream.input, STDOUT_FILENO);
 
         //close unneeded descriptors
-        close(inputStream->input);
-        close(outputStream->output);
+        //close(inputStream.input);
+        //close(outputStream.output);
 
         //execute the other program
         execvp(argv[0], argv);
@@ -50,5 +50,28 @@ void closeStreams(Pipe *pipes, int count) {
  *
  */
 void startChildren(CmdChain commands) {
+    //allocate pipes
+    //pipes between each command, plus an input and output
+    //printf("%d\n", sizeof(Pipe) * (commands.nCmds + 1));
+    Pipe * pipes = (Pipe *)malloc(sizeof(Pipe) * (commands.nCmds + 1));
     
+    for(int i = 0; i < commands.nCmds + 1; i++) {
+        pipe((int*)(pipes + i));
+    }
+
+    pipes[0].input = 0;
+    pipes[0].output = 0;
+    
+    pipes[commands.nCmds].input = 1;
+    pipes[commands.nCmds].output = 1;
+
+    for(int i = 0; i < commands.nCmds + 1; i++) {
+        printf("%d  %d \n", pipes[i].input, pipes[i].output);
+    }
+    
+
+    //now start each child
+    for(int i = 0; i < commands.nCmds; i++) {
+        forkChild(commands.cmds[i].argv, pipes[i], pipes[i + 1]);
+    }
 }
