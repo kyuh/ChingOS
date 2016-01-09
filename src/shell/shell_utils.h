b27b0f765a1a3e306d782d6ee63e403f1d4fc51f
Kevin Yuh <kyuh@caltech.edu>
2016-01-08 21:17:20 -0800

    modify command struct (nCmds), add safe allocator (and associated tests)

--- src/shell/shell_utils.h
+++ src/shell/shell_utils.h
@@ -2,6 +2,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     char **argv;
@@ -12,5 +13,14 @@ typedef struct {
     FILE *inputStream;
     FILE *outputStream;
     CmdInfo *cmds;
+    int nCmds;
 } CmdChain;
 
+void* safeMalloc(size_t nBytes) {
+    void *ptr = malloc(nBytes);
+    if (!ptr){
+        fprintf(stderr, "Memory allocation error, aborting\n");
+        exit(-1);
+    }
+    return ptr;
+}
