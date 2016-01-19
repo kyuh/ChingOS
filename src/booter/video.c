a317846a478ca445d75ab0c5b46f12aacb099de0
Andrew Zhao <azhao@dmail.caltech.edu>
2016-01-19 12:42:46 -0800

    draw text to screen

--- src/booter/video.c
+++ src/booter/video.c
@@ -2,6 +2,7 @@
 #include "handlers.h"
 #include "boot.h"
 #include "assets.h"
+//#include "strings.h"
 
 /* This is the address of the VGA text-mode video buffer.  Note that this
  * buffer actually holds 8 pages of text, but only the first page (page 0)
@@ -31,14 +32,11 @@
  *        position, or any other details you might want to keep track of!
  */
 
+Game_Assets *assets;
+
+
 void color_screen(unsigned char color)
 {
-    char * datas = ((char*) PROGRAM_BASE_ADDR);
-    datas += 100000;
-    datas -= 512;
-
-    Game_Assets * assets = (Game_Assets*)datas;
-
     char* image = &(assets->screen[0]);
     for(int i = 0; i < X_RES * Y_RES; i++)
     {
@@ -49,16 +47,63 @@ void color_screen(unsigned char color)
 char string[] = "Nico Nico Ni";
 char empty[] = " ";
 
+//TODO no good
 void write_string_offset(unsigned char color, char* string, int offset) {
-	volatile char *video = (volatile char*) VIDEO_BUFFER + offset;
+	char *video = (char*) VIDEO_BUFFER + offset;
 	while(*string != 0) {
 		*video++ = *string++;
 		*video++ = color;
 	}
 }
 
+#define CHAR_WIDTH 4
+#define CHAR_HEIGHT 8
+
+void write_char(unsigned char color, char* screen_position, char* char_position)
+{
+    //loop over the font glyph
+    //font page width is 64 pixels
+    for(int x = 0; x < CHAR_WIDTH; x++)
+    {
+        for(int y = 0; y < CHAR_HEIGHT; y++)
+        {
+            if(!char_position[x + y * 64])
+                screen_position[x + y * X_RES] =  color;
+        }
+    }
+}
+
+// TODO bounds checking
+void write_string_position(unsigned char color, char* string, int x, int y)
+{
+    char* font_table = &(assets->font[0]);
+
+	char *video = (char*) VIDEO_BUFFER;
+    char *start_point = video + x + (y * X_RES);
+    
+    while(*string != 0)
+    {
+        char char_code = *string;
+        //get the row and column of the string
+        //there are 16 chars per row
+        //and lots of rows
+        int row = char_code / 16;
+        int col = char_code % 16;
+
+        //now find the location of the char start
+        char* char_position = font_table + col * CHAR_WIDTH + row * 64 * CHAR_HEIGHT;
+
+        write_char(color, start_point, char_position);
+
+        //advance to the next character
+        start_point += CHAR_WIDTH;
+        string++;
+    }
+}
+
+//TODO no good
 void write_string(unsigned char color, char* string) {
-	volatile char *video = (volatile char*) VIDEO_BUFFER;
+	char *video = (char*) VIDEO_BUFFER;
 	while(*string != 0) {
 		*video++ = *string++;
 		*video++ = color;
@@ -66,14 +111,23 @@ void write_string(unsigned char color, char* string) {
 }
 
 void color_pixel(unsigned char color, unsigned short offset) {
-	volatile char *video = (volatile char*) VIDEO_BUFFER + offset;
+	char *video = (char*) VIDEO_BUFFER + offset;
 	*video++ = 219;
 	*video++ = color;
 	// (unsigned char *) VIDEO_BUFFER[offset] = color;
 }
 
+
+
 void init_video(void) {
+    char * datas = ((char*) PROGRAM_BASE_ADDR);
+    datas += 100000;
+    datas -= 512;
+
+    assets = (Game_Assets*)datas;
+
     color_screen(0x3f);
+    write_string_position(5, "wowowow", 315, 5);
     /* TODO:  Do any video display initialization you might want to do, such
      *        as clearing the screen, initializing static variable state, etc.
      */
